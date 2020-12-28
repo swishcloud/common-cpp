e0a31b700e3adfc3c68a34a869957e83a26091be
Your Name <you@example.com>
2020-12-28 10:50:43 +0800

    .

--- common.cpp
+++ common.cpp
@@ -2,26 +2,53 @@
 #include "common.h"
 #include <iostream>
 #include <memory>
+#ifdef __linux__
+#else
 #include <windows.h>
+#endif
 #include <dirent.h>
-wchar_t* common::to_wcstr(const char* orig)
+class common_exception : public std::exception
 {
+public:
+	std::string err;
+	common_exception(std::string err) : err{err}
+	{
+	}
+	virtual const char *what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_USE_NOEXCEPT override
+	{
+		return err.c_str();
+	}
+};
+std::exception PLATFORM_NOT_SUPPORTED()
+{
+	return common_exception("this function not supported on the current platform.");
+}
+wchar_t *common::to_wcstr(const char *orig)
+{
+#ifdef __linux__
+	throw PLATFORM_NOT_SUPPORTED();
+#else
 	size_t newsize = strlen(orig) + 1;
-	wchar_t* wcstring = new wchar_t[newsize];
+	wchar_t *wcstring = new wchar_t[newsize];
 	_locale_t locale = _create_locale(LC_ALL, "zh-CN");
 	size_t convertedChars = _mbstowcs_l(wcstring, orig, newsize, locale);
 	return wcstring;
+#endif
 }
-char* common::to_cstr(const wchar_t* orig)
+char *common::to_cstr(const wchar_t *orig)
 {
+#ifdef __linux__
+	throw PLATFORM_NOT_SUPPORTED();
+#else
 	size_t origsize = wcslen(orig) + 1;
 	const size_t newsize = origsize * 2;
-	char* cstring = new char[newsize];
+	char *cstring = new char[newsize];
 	_locale_t locale = _create_locale(LC_ALL, "zh-CN");
 	_wcstombs_l(cstring, orig, newsize, locale);
 	return cstring;
+#endif
 }
-std::string common::to_string(const wchar_t* orig)
+std::string common::to_string(const wchar_t *orig)
 {
 	std::wstring ws(orig);
 	std::string str(ws.begin(), ws.end());
@@ -29,38 +56,65 @@ std::string common::to_string(const wchar_t* orig)
 }
 std::wstring common::full_path(std::wstring path)
 {
+#ifdef __linux__
+	throw PLATFORM_NOT_SUPPORTED();
+#else
 	const int BUFSIZE = 4096;
 	wchar_t buf[BUFSIZE]{};
 	GetFullPathNameW(path.c_str(), BUFSIZE, buf, NULL);
 	return std::wstring(buf);
+#endif
 }
 
-int common::file_exist(const char* filename)
+int common::file_exist(const char *filename)
 {
+#ifdef __linux__
+	if (!std::filesystem::exists(filename))
+	{
+		return 0;
+	}
+	else if (std::filesystem::is_directory(filename))
+	{
+		return 1;
+	}
+	else
+	{
+		return 2;
+	}
+#else
 	WIN32_FILE_ATTRIBUTE_DATA info{};
 	if (GetFileAttributesExA(filename, GET_FILEEX_INFO_LEVELS(), &info) == 0)
 	{
 		return 0;
 	}
-	if (info.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+	if (info.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+	{
 		return 1;
 	}
 	return 2;
+#endif
 }
-void common::run_file(const wchar_t* filename)
+void common::run_file(const wchar_t *filename)
 {
+#ifdef __linux__
+	throw PLATFORM_NOT_SUPPORTED();
+#else
 	ShellExecuteW(GetForegroundWindow(), L"open", filename, NULL, NULL, SW_SHOWNORMAL);
+#endif
 }
-int common::find_files(std::wstring path, std::vector<std::wstring>& files)
+int common::find_files(std::wstring path, std::vector<std::wstring> &files)
 {
-	_WDIR* dir;
-	struct _wdirent* ent;
+#ifdef __linux__
+	throw PLATFORM_NOT_SUPPORTED();
+#else
+	_WDIR *dir;
+	struct _wdirent *ent;
 	if ((dir = _wopendir(path.c_str())) != NULL)
 	{
 		int count = 0;
 		while ((ent = _wreaddir(dir)) != NULL)
 		{
-			wchar_t* file_name_w = ent->d_name;
+			wchar_t *file_name_w = ent->d_name;
 			files.push_back(std::wstring(file_name_w));
 			++count;
 		}
@@ -72,18 +126,19 @@ int common::find_files(std::wstring path, std::vector<std::wstring>& files)
 	{
 		return -1;
 	}
+#endif
 }
-int common::find_files(std::string path, std::vector<std::string>& files)
+int common::find_files(std::string path, std::vector<std::string> &files)
 {
-	DIR* dir;
-	struct dirent* ent;
+	DIR *dir;
+	struct dirent *ent;
 	if ((dir = opendir(path.c_str())) != NULL)
 	{
 		int count = 0;
 		while ((ent = readdir(dir)) != NULL)
 		{
-			char* file_name = ent->d_name;
-			if (strcmp(file_name, ".")==0 || strcmp(file_name, "..") == 0)
+			char *file_name = ent->d_name;
+			if (strcmp(file_name, ".") == 0 || strcmp(file_name, "..") == 0)
 			{
 				continue;
 			}
@@ -99,12 +154,14 @@ int common::find_files(std::string path, std::vector<std::string>& files)
 		return -1;
 	}
 }
-char* common::strncpy(const char* source) {
-	if (!source) {
+char *common::strncpy(const char *source)
+{
+	if (!source)
+	{
 		return NULL;
 	}
 	int size = strlen(source) + 1;
-	char* const cpy = new char[size];
+	char *const cpy = new char[size];
 	::strncpy(cpy, source, size);
 	return cpy;
 }
--- include/common.h
+++ include/common.h
@@ -6,24 +6,25 @@
 #include <iostream>
 #include <memory>
 #include <cstring>
+#include <filesystem>
 namespace common
 {
 	template <typename... Arg>
-	std::string string_format(const std::string& format, Arg... args);
-	wchar_t* to_wcstr(const char* orig);
-	char* to_cstr(const wchar_t* orig);
-	std::string to_string(const wchar_t* orig);
+	std::string string_format(const std::string &format, Arg... args);
+	wchar_t *to_wcstr(const char *orig);
+	char *to_cstr(const wchar_t *orig);
+	std::string to_string(const wchar_t *orig);
 	std::wstring full_path(std::wstring path);
 	//result: 0 does not exist;1 directory;2 file.
-	int  file_exist(const char* filename);
-	void run_file(const wchar_t* filename);
-	int find_files(std::wstring path, std::vector<std::wstring>& files);
-	int find_files(std::string path, std::vector<std::string>& files);
-	char* strncpy(const char* source);
+	int file_exist(const char *filename);
+	void run_file(const wchar_t *filename);
+	int find_files(std::wstring path, std::vector<std::wstring> &files);
+	int find_files(std::string path, std::vector<std::string> &files);
+	char *strncpy(const char *source);
 } // namespace common
 
 template <typename... Arg>
-std::string common::string_format(const std::string& format, Arg... args)
+std::string common::string_format(const std::string &format, Arg... args)
 {
 	size_t size = snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
 	if (size <= 0)
